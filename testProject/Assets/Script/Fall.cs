5162d2497b2e0baaa7c08965b254f69933f1948e
alliegoober <alliegoober@gmail.com>
2014-10-15 17:36:38 -0700

    updated fall script

--- testProject/Assets/Script/Fall.cs
+++ testProject/Assets/Script/Fall.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class Fall : MonoBehaviour {
 
 	Vector3 startPosition;
+	public float minimumHeight = -10;
+
 	// Use this for initialization
 	void Start () {
 		startPosition = transform.position;
@@ -12,7 +14,7 @@ public class Fall : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(transform.position.y < -10)
+		if(transform.position.y < minimumHeight)
 		{
 			transform.position= startPosition;
 		}
