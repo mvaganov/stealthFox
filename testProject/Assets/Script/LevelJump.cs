af806956adf6b53dd2f113732811c4dc7a6f4b5e
Michael Vaganov <mvaganov@hotmail.com>
2014-10-15 17:22:58 -0700

    created a level jump script

---
+++ testProject/Assets/Script/LevelJump.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelJump : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		// asks the program if the 'e' key was pressed
+		if(Input.GetKeyDown(KeyCode.E))
+		{
+			// go to Elise's level if 'e' was pressed
+			Application.LoadLevel("Elise");
+		}
+
+		if(Input.GetKeyDown(KeyCode.R))
+		{
+			Application.LoadLevel("Rachel");
+		}
+
+		if(Input.GetKeyDown(KeyCode.T))
+		{
+			Application.LoadLevel("Allie");
+		}
+	}
+}
