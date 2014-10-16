90d36773264e784215fe5d568c53354c8f36233e
Michael Vaganov <mvaganov@hotmail.com>
2014-10-15 17:44:49 -0700

    prefab + create script

---
+++ testProject/Assets/Script/CreateObject.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateObject : MonoBehaviour {
+
+	public GameObject whatToCreate;
+	public KeyCode whichKeyCreatesIt = KeyCode.Alpha1;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(whichKeyCreatesIt))
+		{
+			// create the thing I want right in front of me
+			Instantiate(whatToCreate, 
+			            transform.position + transform.forward,
+			            // facing the same direction as me.
+			            transform.rotation);
+		}
+	}
+}
