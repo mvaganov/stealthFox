8e27d1e831b3898756163aa74868cf9850f61150
Michael Vaganov <mvaganov@hotmail.com>
2014-10-15 17:51:48 -0700

    added extra script variable

--- testProject/Assets/Script/CreateObject.cs
+++ testProject/Assets/Script/CreateObject.cs
@@ -5,6 +5,7 @@ public class CreateObject : MonoBehaviour {
 
 	public GameObject whatToCreate;
 	public KeyCode whichKeyCreatesIt = KeyCode.Alpha1;
+	public float distanceInFront = 2;
 	// Use this for initialization
 	void Start () {
 	
@@ -16,7 +17,7 @@ public class CreateObject : MonoBehaviour {
 		{
 			// create the thing I want right in front of me
 			Instantiate(whatToCreate, 
-			            transform.position + transform.forward,
+			            transform.position + transform.forward * distanceInFront,
 			            // facing the same direction as me.
 			            transform.rotation);
 		}
