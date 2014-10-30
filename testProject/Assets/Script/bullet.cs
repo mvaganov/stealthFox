f3c776a020c04dfb0e4170cf75216489787e77c7
Chaiyawat Nunes <chaiyawat.nunes@gmail.com>
2014-10-29 17:27:25 -0700

    Wattie
    -

---
+++ testProject/Assets/Script/bullet.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Rigidbody))]
+public class bullet : MonoBehaviour {
+
+	public float speed = 100;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody.velocity = transform.forward * speed;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
