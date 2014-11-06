f0d9fd1a2408bf82d94a89cb497fc8b55129b5cb
Chaiyawat Nunes <chaiyawat.nunes@gmail.com>
2014-11-05 16:40:39 -0800

    Update 11/5/14

---
+++ testProject/Assets/Script/diesToBullet.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class diesToBullet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision col) {
+		bullet b = col.gameObject.GetComponent<bullet> ();
+		if (b != null) {
+			renderer.material.color = new Color(
+				Random.Range(0,1f),
+				Random.Range(0,1f),
+				Random.Range(0,1f));
+		}
+	}
+}
