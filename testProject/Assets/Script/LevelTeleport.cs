34ab2a0ddf183c1474c681c59307be3594263e25
Michael Vaganov <mvaganov@hotmail.com>
2014-11-05 18:08:30 -0800

    teleporter
    * now we can teleport between levels!

---
+++ testProject/Assets/Script/LevelTeleport.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelTeleport : MonoBehaviour {
+
+	public string levelName;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		CharacterController cc = other.gameObject.GetComponent<CharacterController> ();
+		if(cc != null) {
+			Application.LoadLevel(levelName);
+		}
+	}
+}
