6a2a0f16bd86c8b7b55304775f53fd4ce281b175
Chaiyawat Nunes <chaiyawat.nunes@gmail.com>
2014-11-19 16:19:39 -0800

    Update

--- testProject/Assets/Script/LightStrobe2.cs
+++ testProject/Assets/Script/LightStrobe2.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class LightStrobe2 : MonoBehaviour {
 	public float intensityStart = 0, intensityEnd = 2;
-	public float duration = 3;
+	public float duration = 0.01f;
 	public float offDuration;
 	public float reverseDuration = 0.01f;
 	public bool reverseAlso = true;
@@ -21,23 +21,22 @@ public class LightStrobe2 : MonoBehaviour {
 			if(!reversing && doneReversing == false) {
 				light.intensity = 
 					totalIntensityChange * timer / duration+ intensityStart;
-			} else if (!reversing  && doneReversing == false){
-				light.intensity = 
-					totalIntensityChange * timer / reverseDuration+ intensityStart;
-			} else if (reversing && doneReversing == true){
-			light.intensity = 
-				totalIntensityChange * timer / offDuration+ intensityStart;
-			} else {
+			} 
+//			else if (!reversing  && doneReversing == false){
+//				light.intensity = 
+//					totalIntensityChange * timer / reverseDuration+ intensityStart;
+//			} else if (reversing && doneReversing == true){
+//			light.intensity = 
+//				totalIntensityChange * timer / offDuration+ intensityStart;
+//			} 
+		else {
 				light.intensity = 
 					totalIntensityChange * (reverseDuration-timer) / reverseDuration+ intensityStart;
 			}
 		} else {
 			if(reverseAlso) {
-				duration = Random.Range (0,2);
-				offDuration = Random.Range (0,1);
 				timer = 0;
 				reversing = !reversing;
-				doneReversing = true;
 			}
 		}
 		//timer = timer + Time.deltaTime;
